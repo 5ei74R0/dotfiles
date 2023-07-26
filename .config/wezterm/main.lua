fe1b78399af5c8e56aa1a533d5f76c21bbf5dbce
5ei74R0 <otoxukk.y@gmail.com>
2023-07-26 23:21:01 +0900

    :zap: Update wezterm config

--- .config/wezterm/main.lua
+++ .config/wezterm/main.lua
@@ -12,8 +12,36 @@ end
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font 'UDEV Gothic 35NFLG'
-config.font_size = 13.0
+config.font = wezterm.font { family = 'UDEV Gothic 35NFLG', weight = 'Bold' }
+config.font_size = 13.5
+
+use_fancy_tab_bar = false
+
+config.window_background_opacity = 0.95
+config.window_background_gradient = {
+  colors = { '#002916', '#050633', '#37040e' },
+  -- colors = { '#003c29', '#302b63', '#5c243e' },
+  orientation = {
+    Radial = {
+      -- Specifies the x coordinate of the center of the circle,
+      -- in the range 0.0 through 1.0.  The default is 0.5 which
+      -- is centered in the X dimension.
+      cx = 0.25,
+
+      -- Specifies the y coordinate of the center of the circle,
+      -- in the range 0.0 through 1.0.  The default is 0.5 which
+      -- is centered in the Y dimension.
+      cy = 0.25,
+
+      -- Specifies the radius of the notional circle.
+      -- The default is 0.5, which combined with the default cx
+      -- and cy values places the circle in the center of the
+      -- window, with the edges touching the window edges.
+      -- Values larger than 1 are possible.
+      radius = 1.30,
+    },
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
