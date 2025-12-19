6244d8788c762533c15083a80127ad784617548c
Seitaro Otsuki <56509932+5ei74R0@users.noreply.github.com>
2025-12-19 16:47:03 +0900

    fix: setting of history

--- .config/zsh/sync/options.zsh
+++ .config/zsh/sync/options.zsh
@@ -1,6 +1,9 @@
-HISTSIZE=1000
-SAVEHIST=10000
-HISTFILE=~/.zsh_history
-
-setopt histignorealldups
-setopt sharehistory
+# HISTORY
+export HISTFILE=${HOME}/.zsh_history
+export HISTSIZE=10000  # history size in memory
+export SAVEHIST=10000  # history size on disk
+setopt share_history          # share history across all sessions
+setopt append_history         # append to the history file, don't overwrite it
+setopt inc_append_history     # write to the history file immediately, not when the shell exits
+setopt hist_ignore_all_dups   # remove older duplicates from the history list
+setopt hist_reduce_blanks     # remove superfluous blanks from each command line
